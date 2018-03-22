91909927b50731898d1115bbd89ef817dcc2a48f
David <davidskiold@yahoo.se>
2018-03-22 21:30:27 +0100

    Forts. Arkitektur
    GameObject träd fortsättning

---
+++ CoT/CoT/Game Object Inheritance/Physics/Particle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace CoT {
+    public class Particle : PhysicsObject {
+        public Particle(Vector2 position) : base(position) {
+        }
+    }
+}
