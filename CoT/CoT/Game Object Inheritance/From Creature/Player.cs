e9cd909bbc184c43155bf72c1eb1022ed5dc02d2
Thegemsearcher <jacklewitan@hotmail.com>
2018-03-27 18:14:36 +0200

    PC karaktären men dens movement funkar inte just nu. Dens metod/Update kallas ej på heller för tillfället.

--- CoT/CoT/Game Object Inheritance/From Creature/Player.cs
+++ CoT/CoT/Game Object Inheritance/From Creature/Player.cs
@@ -4,16 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace CoT {
     class Player : Creature {
         enum heroClass {
 
         }
+        public Texture2D pC;
+        MouseState mS;
+        Vector2 mousePosition;
+        Vector2 position;
+
+
         public Player(Vector2 position) : base(position) {
+            this.position = position;
         }
         public override void Update(GameTime gameTime) {
 
+
+            if (mS.LeftButton == ButtonState.Pressed && mS.LeftButton != ButtonState.Released && mousePosition != position)
+            {
+                position = mousePosition;
+            }
         }
     }
 }
