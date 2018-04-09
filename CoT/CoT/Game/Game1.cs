3b72f53038827737a5a4d336842ffb3ea5ef8098
Adrian <adrian.carlstrom@gmail.com>
2018-04-09 10:21:16 +0200

    Revert "Revert "merge branch2 to master""
    This reverts commit f1810068d90491d23f06a67c51d868a6386f188a.

--- CoT/CoT/Game/Game1.cs
+++ CoT/CoT/Game/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Windows.Forms;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -8,21 +9,34 @@ using MonoGame.Extended;
 using Myra;
 using Myra.Graphics2D.UI;
 using Penumbra;
+using Button = Myra.Graphics2D.UI.Button;
+using ComboBox = Myra.Graphics2D.UI.ComboBox;
+using Console = System.Console;
 
 
 namespace CoT
 {
     public class Game1 : Game
     {
+        #region Constants
+
+        public static readonly int ScreenWidth = 1920;
+        public static readonly int ScreenHeight = 1080;
+        public static readonly Vector2 TileOffset = new Vector2(-0.5f, 0.5f);
+        public static readonly int MonitorWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+        public static readonly int MonitorHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+
+        #endregion
+
+        #region Fields
+
         public static Game1 Game { get; set; }
         public GraphicsDeviceManager Graphics { get; set; }
+        public Form WindowForm { get; set; }
         public GameManager GameManager { get; set; }
+        public Desktop host = new Desktop();
 
-        public static readonly int ScreenWidth = 1280;
-        public static readonly int ScreenHeight = 720;
-        public static readonly Vector2 TileOffset = new Vector2(-0.5f, 0.5f);
-
-        public Desktop host;
+        #endregion
 
         public Game1()
         {
@@ -38,100 +52,54 @@ namespace CoT
             Graphics.PreferredBackBufferHeight = ScreenHeight;
             Graphics.IsFullScreen = false;
 
-            var form = (System.Windows.Forms.Form)System.Windows.Forms.Control.FromHandle(this.Window.Handle);
-            form.Location = new System.Drawing.Point(0, 0);
+            WindowForm = (Form)Control.FromHandle(Window.Handle);
+            Window.AllowAltF4 = true;
+            Window.AllowUserResizing = false;
+            Window.IsBorderless = false;
+            Window.Position = new Point(WindowBorderSize.X + 1, 0);
             Graphics.ApplyChanges();
 
-            GameManager = new GameManager(this);
-            Components.Add(GameManager);
+            Console.WriteLine("Game1 - Constructor");
         }
+
+        public Point WindowBorderSize => new Point(
+                WindowForm.RectangleToScreen(WindowForm.ClientRectangle).Right - WindowForm.Right,
+                WindowForm.RectangleToScreen(WindowForm.ClientRectangle).Top - WindowForm.Top);
 
         protected override void Initialize()
         {
-            Console.WriteLine("Init - Game1");
+            Console.WriteLine("Game1 - Initialize");
+
+            MyraEnvironment.Game = this;
+
+            GameManager = new GameManager();
+            GameManager.Initialize();
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            Console.WriteLine("LoadContent - Game1");
-            #region GUI
-            MyraEnvironment.Game = this;
-
-            var grid = new Grid
-            {
-                RowSpacing = 8,
-                ColumnSpacing = 8
-            };
-
-            grid.ColumnsProportions.Add(new Grid.Proportion(Grid.ProportionType.Auto));
-            grid.ColumnsProportions.Add(new Grid.Proportion(Grid.ProportionType.Auto));
-            grid.RowsProportions.Add(new Grid.Proportion(Grid.ProportionType.Auto));
-            grid.RowsProportions.Add(new Grid.Proportion(Grid.ProportionType.Auto));
-
-            // TextBlock
-            var helloWorld = new TextBlock
-            {
-                Text = "Hello, World!"
-            };
-            grid.Widgets.Add(helloWorld);
-
-            // ComboBox
-            var combo = new ComboBox
-            {
-                GridPositionX = 1,
-                GridPositionY = 0
-            };
-
-            combo.Items.Add(new ListItem("Red", Color.Red));
-            combo.Items.Add(new ListItem("Green", Color.Green));
-            combo.Items.Add(new ListItem("Blue", Color.Blue));
-            grid.Widgets.Add(combo);
-
-            // Button
-            var button = new Button
-            {
-                GridPositionX = 0,
-                GridPositionY = 1,
-                Text = "Show"
-            };
-
-            button.Down += (s, a) =>
-            {
-                var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
-                messageBox.ShowModal(host);
-            };
-
-            grid.Widgets.Add(button);
-
-            // Spin button
-            var spinButton = new SpinButton
-            {
-                GridPositionX = 1,
-                GridPositionY = 1,
-                WidthHint = 100,
-                Nullable = true
-            };
-            grid.Widgets.Add(spinButton);
-
-            // Add it to the desktop
-            host = new Desktop();
-            host.Widgets.Add(grid);
-            #endregion
+            Console.WriteLine("Game1 - LoadContent");
+            GameManager.LoadContent();
+            base.LoadContent();
         }
         protected override void UnloadContent()
         {
-            Console.WriteLine("UnloadContent - Game1");
+            Console.WriteLine("Game1 - UnloadContent");
+            GameManager.UnloadContent();
             base.UnloadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
+            GameManager.Update(gameTime);
             base.Update(gameTime);
         }
         protected override void Draw(GameTime gameTime)
         {
             base.Draw(gameTime);
+            GameManager.Draw(gameTime);
             host.Bounds = new Rectangle(0, 0, GraphicsDevice.PresentationParameters.BackBufferWidth, GraphicsDevice.PresentationParameters.BackBufferHeight);
             host.Render();
         }
--- CoT/CoT/GameObjects/Creatures/Boss.cs
+++ CoT/CoT/GameObjects/Creatures/Boss.cs
@@ -11,7 +11,7 @@ namespace CoT
 {
     public class Boss : Enemy
     {
-        public Boss(string texture, Vector2 position, Rectangle sourceRectangle, Player player, Grid grid) : base(texture, position, sourceRectangle, player, grid)
+        public Boss(string texture, Vector2 position, Rectangle sourceRectangle, Player player, Grid grid, Map map) : base(texture, position, sourceRectangle, player, grid, map)
         {
         }
 
