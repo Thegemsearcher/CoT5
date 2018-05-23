a04dd94b9240100e96a4a3d7b6ab90c9478ae5e1
David <davidskiold@yahoo.se>
2018-05-23 22:12:42 +0200

    ytterligare Hud mest text för items

--- CoT/CoT/HuD.cs
+++ CoT/CoT/HuD.cs
@@ -8,8 +8,8 @@ namespace CoT
         private Player player;
         private SpriteFont font = ResourceManager.Get<SpriteFont>("font1");
         private Vector2 infoPos, hitBoxOffset = new Vector2(-50,-50);
-        private bool hoverOver = false;
-        private Vector2 position = new Vector2(0,0);
+        private bool hoverOverOnGround = false;
+        private Vector2 infoPosition = new Vector2(0,0), textAtMouse = new Vector2(0,0);
         private GameObject gameObject;//det gameobject som vi vill visa info från
         private string fullInfoBox = "";
         private FloatRectangle mouseHitBox = new FloatRectangle(new Vector2(0,0), new Vector2(55,55));
@@ -26,30 +26,37 @@ namespace CoT
             //position.Y = Input.CurrentMousePosition.Y;
             foreach (Item item in ItemManager.Instance.Items)
             {
-                if (mouseHitBox.Contains(item.Position.WorldToScreen()))
+                if (mouseHitBox.Contains(item.Position.WorldToScreen()) || mouseHitBox.Contains(new Vector2(item.rectItemInv.X,item.rectItemInv.Y)))
                 {
-                    position.X = 50;
-                    position.Y = 100;
-                    hoverOver = true;
+                    infoPosition.X = 50;
+                    infoPosition.Y = 100;
                     gameObject = item;
+                    if (!item.isInBag)
+                    {
+                        PickUpInstruction();
+                    }
+                    else
+                    {
+                        hoverOverOnGround = false;
+                    }
                     if (item is Potion p)
                     {
                         switch (p.currentPotionType)
                         {
                             case Potion.PotionType.FireBall:
-                                fullInfoBox = "item type: Fireball Potion" + " \n " + "On Use: you get two fireballs which can be used by pressing 1 and then attacking" + " \n " + "Use by hovering over the item and pressing RIGHT CLICK";
+                                fullInfoBox = "item type: Fireball Potion" + "\n" + "On Use: you get two fireballs which can be used by pressing 1 and then attacking" + "\n" + "Use by hovering over the item and pressing RIGHT CLICK";
                                 break;
                             case Potion.PotionType.HealthLarge:
-                                fullInfoBox = "item type: Large Health Potion" + " \n " + " On Use: You gain 80 hp" + " \n " + "Use by hovering over the item and pressing RIGHT CLICK";
+                                fullInfoBox = "item type: Large Health Potion" + "\n" + " On Use: You gain 80 hp" + "\n" + "Use by hovering over the item and pressing RIGHT CLICK";
                                 break;
                             case Potion.PotionType.HealthMedium:
                                 fullInfoBox = "item type: Medium Health Potion \nOn Use: Uou gain 40 hp \nUse by hovering over the item and pressing RIGHT CLICK";
                                 break;
                             case Potion.PotionType.HealthSmall:
-                                fullInfoBox = "item type: Small Health Potion" + " \n " + " On Use: You gain 20 hp" + " \n " + "Use by hovering over the item and pressing RIGHT CLICK";
+                                fullInfoBox = "item type: Small Health Potion" + "\n" + " On Use: You gain 20 hp" + "\n" + "Use by hovering over the item and pressing RIGHT CLICK";
                                 break;
                             case Potion.PotionType.SpeedPotion:
-                                fullInfoBox = "item type: Speed Potion" + " \n " + " On Use: Your speed doubles, OBS: DOES NOT STACK" + " \n " + "Use by hovering over the item and pressing RIGHT CLICK";
+                                fullInfoBox = "item type: Speed Potion" + "\n" + " On Use: Your speed doubles, OBS: DOES NOT STACK" + "\n" + "Use by hovering over the item and pressing RIGHT CLICK";
                                 break;
                             default:
                                 break;
@@ -64,7 +71,17 @@ namespace CoT
             //    sb.DrawString(font, "" + gameObject.Hitbox.Size.X + "    " + gameObject.Hitbox.Size.Y, position, Color.Yellow, 0, new Vector2(0, 0), 0.8f, SpriteEffects.None, 0.9f);
             //}
             //sb.Draw(ResourceManager.Get<Texture2D>("rectangle"), mouseHitBox, null, Color.Magenta * 0.3f, 0, Vector2.Zero, SpriteEffects.None, 0.9f);
-            sb.DrawString(font, fullInfoBox, position, Color.Yellow, 0, new Vector2(0,0), 0.8f, SpriteEffects.None, 0.9f);
+            sb.DrawString(font, fullInfoBox, infoPosition, Color.Yellow, 0, new Vector2(0,0), 0.8f, SpriteEffects.None, 0.9f);
+            if (hoverOverOnGround)
+            {
+                sb.DrawString(font, "E to Pick up", textAtMouse, Color.Yellow, 0, new Vector2(0, 0), 0.6f, SpriteEffects.None, 0.8f);
+            }
+        }
+        public void PickUpInstruction()
+        {
+            hoverOverOnGround = true;
+            textAtMouse.X = Input.CurrentMousePosition.X + 25;
+            textAtMouse.Y = Input.CurrentMousePosition.Y;
         }
     }
 }
